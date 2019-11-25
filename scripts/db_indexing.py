3232e1ff83df136a1e7544735c9afce139143008
Vincent Berthet <vincent.berthet42@gmail.com>
2019-11-25 18:08:34 +0100

    change path in db_indexing.py

--- scripts/db_indexing.py
+++ scripts/db_indexing.py
@@ -1,10 +1,9 @@
 #export PYTHONPATH="/usr/local/opencv-2.4.9/lib/python2.7/site-packages/"
 
-
 import os
 import sys
 import glob
-import cv2
+import cv2 as cv
 import numpy as np
 import argparse
 
@@ -29,12 +28,12 @@ parser.add_argument("-d", "--database", dest="db_name",
 args = parser.parse_args()
 
 ## Set paths
-img_dir="/share/esir3/VO/Images/" + args.db_name + "/"
+img_dir="../sources/" + args.db_name + "/"
 imagesNameList = glob.glob(img_dir+"*.jpg")
-output_dir="./results/" + args.db_name
+output_dir="./output/" + args.db_name
 
 if not os.path.exists(img_dir):
-    print "The directory containing images: "+img_dir+" is not found -- EXIT\n"
+    print ("The directory containing images: "+img_dir+" is not found -- EXIT\n")
     sys.exit(1)
 
 
